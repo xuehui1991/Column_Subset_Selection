347ee6bcbee038f43bcad0c4391182cf6244701b
rustle1314 <lili1987mail@gmail.com>
2015-01-20 11:27:45 +0000

    add test for approSVD

--- approSVD/approSVD.py
+++ approSVD/approSVD.py
@@ -5,19 +5,22 @@ import sys;
 util_dir  = os.path.split(os.path.realpath(__file__))[0] + "/../utils/Python_Utils";
 util_dir1 = os.path.split(os.path.realpath(__file__))[0] + "/../Python_Utils";
 sys.path.append(util_dir);
-sys.path.append(rrqr_dir);
+sys.path.append(util_dir1);
+
 
 import numpy      as np;
 import algorithm1 as greedy;
 from Matrix_Utils import *;
 
+is_debug = False;
 
-def css(A, k, delta=0.1):
-    m,n    = A.shape;
+def css(A, k, delta = 0.1):
+    greedy.is_debug = is_debug;
+    m,n             = A.shape;
 
-    v,d,vt = np.linalg.svd(A);
+    u,d,vt = np.linalg.svd(A);
     Ak     = np.dot(u[:,0:k], vt[0:k,:]);
-    isPass, Lambda = greedy(A, Ak, delta);
+    isPass, Lambda = greedy.greedy(A, Ak, delta);
     
     C   = [];
     for i in xrange(len(Lambda)):
